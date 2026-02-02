b8b3507f1182592b5af7b0212d77f50bd0a6ecea
ppedvLukasK <lukask@ppedv.de>
2026-02-02 12:33:44 +0100

    M003, M004

---
+++ M004/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿namespace M004;
+
+public partial class MainPage : ContentPage
+{
+	private int count;
+
+	private double value;
+
+	public MainPage()
+	{
+		InitializeComponent();
+
+		Zahlen.ItemsSource = Enumerable.Range(0, 10).ToList();
+	}
+
+	private void Button_Clicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+	}
+
+	private void SearchBar_SearchButtonPressed(object sender, EventArgs e)
+	{
+
+    }
+
+	private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+	{
+		value = e.NewValue;
+		Output.Text = value.ToString();
+	}
+}
