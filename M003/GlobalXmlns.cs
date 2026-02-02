e2bbe6ca93625561f61f1a8f5daf9e64bde96568
ppedvLukasK <lukask@ppedv.de>
2026-02-02 11:31:15 +0100

    M003

---
+++ M003/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "M003")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "M003.Pages")]
---
+++ M003/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace M003
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : MauiUIApplicationDelegate
+	{
+		protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+	}
+}
